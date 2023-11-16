5f055f12840a1f82a73506a88088e2536e70f475
CE Abdulrahman Ismael <k.u.r.d.n.e.t.547@gmail.com>
2023-11-17 00:31:28 +0300

    deploy

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+/* ================== typing animation ====================== */
+var typed = new Typed(".typing", {
+    strings:["IT Project Manager","Web Designer","IT Administrator","Accounting","IT Support","","IT Security","","IT Systems Administrator","","IT Systems Analyst","","IT Systems Engineer","","IT Team Leader","","IT Trainer"],
+    typeSpeed: 100,
+    backSpeed: 40,
+    loop: true
+});
