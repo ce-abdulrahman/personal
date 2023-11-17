aa8573b0f3ec00a2f4fafce7a2db9647f4f30225
CE Abdulrahman Ismael <79444459+ce-abdulrahman@users.noreply.github.com>
2023-11-17 21:13:42 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 /* ================== typing animation ====================== */
 var typed = new Typed(".typing", {
-    strings:["IT Project Manager","Web Designer","IT Administrator","Accounting","IT Support","","IT Security","","IT Systems Administrator","","IT Systems Analyst","","IT Systems Engineer","","IT Team Leader","","IT Trainer"],
+    strings:["IT Project Manager","Web Designer","IT Support","Database Administration","IT Security","Accounting System Manager","IT Systems Administrator","","IT Systems Analyst","","IT Systems Engineer","","IT Team Leader","","IT Trainer"],
     typeSpeed: 100,
     backSpeed: 40,
     loop: true
